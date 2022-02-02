22e8b1a2e8ae70b6b9bf34606e335441d5dfa1bf
Cayla Dedrick <ckd5370@psu.edu>
2022-02-01 21:49:46 -0500

    homework 2 done

--- astro530.py
+++ astro530.py
@@ -21,3 +21,22 @@ def Planck(wn, T):
     B_nu = 2 * c.h * nu ** 3 / c.c ** 2 * (np.exp(c.h * c.c * wn / (c.k_B * T)) - 1) ** -1
     B_nu = B_nu.to(u.erg / u.s / u.cm ** 2 / u.Hz)
     return B_nu.value
+
+### HW 2
+
+def _trapz(y, x):
+    # Integrate numerically using the trapezoid rule
+    area = 0
+    for i in range(len(x) - 1):
+        dx = x[i+1] - x[i]
+        area += (y[i] + y[i+1]) / 2 * dx 
+    return area
+
+def NIntegrate(func, a, b, dx, integrator = _trapz, **kwargs):
+    '''
+        numerically integrate 'func' from x = a to x = b with step size dx 
+    '''
+    n = int((b - a) * dx)
+    x = 10 ** np.linspace(a, b, n)
+    y = func(x, **kwargs)
+    return _trapz(y, x)
