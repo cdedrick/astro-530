638bf14de9bad995e9d6f7b84b1200fa0cb1180d
Cayla Dedrick <ckd5370@psu.edu>
2022-05-31 15:30:48 -0400

    update dump

--- astro530.py
+++ astro530.py
@@ -1,12 +1,41 @@
 import numpy as np
+import pandas as pd
 
 from astropy import units as u
 from astropy import constants as c
 from astropy.modeling import models
+from scipy.integrate import trapz
 from scipy.interpolate import interp1d
+from scipy.special import wofz
 
+e = c.e.esu
 
 ### HW 1
+
+A_table = pd.read_pickle('./data/abundances.pkl')
+i_table = pd.read_pickle('./data/ionization.pkl')
+u_table = pd.read_pickle('./data/partition.pkl')
+
+VALIIIC = pd.read_pickle('./data/VALIIIC_atm.pkl')
+
+tau_500 = VALIIIC['tau_500'].to_numpy()
+
+h = VALIIIC['h'].to_numpy() * u.km
+s = h[0] - h
+
+T = VALIIIC['T'].to_numpy() * u.K
+
+micro = VALIIIC['V'].to_numpy() * u.km/u.s
+
+nH = VALIIIC['n_H'].to_numpy() * u.cm**-3
+
+ne = VALIIIC['n_e'].to_numpy() * u.cm**-3
+Pe = ne * c.k_B * T
+
+Pg = VALIIIC['Pgas/Ptotal'].to_numpy() * VALIIIC['Ptotal'].to_numpy() * u.barye
+
+rho = VALIIIC['rho'].to_numpy() * u.g/u.cm**3
+
 
 def Planck(wn, T):
     '''
@@ -228,15 +257,15 @@ def K_e(Pg, Pe, A):
     a_e = c.sigma_T
     return (a_e * Pe /(Pg-Pe) * np.sum(A)).cgs
 
-def K_total(l, T, Pg, Pe, A_table, i_table, u_table, elements, verbose=False):
+def Kc_total(l, T, Pg, Pe, A_table = A_table, i_table=i_table, u_table=u_table, verbose=False):
 #    Pe = P_e(T, Pg, A_table, i_table, u_table, elements)
     Phi_H = saha_phi('H', T, i_table, u_table)
     
     neutral_H = 1 / (1 + Phi_H / Pe)
     K_hydrogen = KH(l, T, Pe, i_table)
     
-    A_j = A_table.loc[elements]['A'].to_numpy()
-    mmm = np.sum(A_table.loc[elements]['A'] * A_table.loc[elements]['weight'] * u.u).cgs
+    A_j = A_table['A'].to_numpy()
+    mmm = np.sum(A_table['A'] * A_table['weight'] * u.u).cgs
     
     K_electron = K_e(Pg, Pe, A_j)
     total_per_H = K_hydrogen * neutral_H + K_electron
@@ -251,3 +280,83 @@ def K_total(l, T, Pg, Pe, A_table, i_table, u_table, elements, verbose=False):
         print('K(e-) = {:.4g}'.format(K_electron / mmm))
         print('\n')
     return total_per_g
+
+### HW 10
+def gamma_4(T, Pe, log_C4):
+    Pe = Pe / u.barye
+    T = T / u.K
+    log_gamma4 = 19 + 2/3 * log_C4 + np.log10(Pe) - 5/6 * np.log10(T)
+    return 10 ** log_gamma4 * 1/u.s
+
+def gamma_6(T, Pg, log_C6):
+    Pg = Pg / u.barye
+    T = T / u.K
+    log_gamma6 = 20 + 0.4 * log_C6 + np.log10(Pg) - 0.7 * np.log10(T)
+    return 10 ** log_gamma6 * 1/u.s
+
+
+def gamma(T, Pg, Pe, A_ul,log_C4, log_C6):
+    gamma_rad = A_ul
+    gamma4 = gamma_4(T, Pe,log_C4)
+    gamma6 = gamma_6(T, Pg, log_C6)
+    return gamma_rad + gamma4 + gamma6
+
+def gaussian_width(l0, T, micro):
+    m_Na = 22.99 * u.u
+    therm = 2 * c.k_B * T / m_Na
+    return l0 / c.c * np.sqrt(therm + micro**2)
+
+def Hjerting(u, a):
+    return np.real(wofz(u + 1j*a)) 
+
+def osc_f(l, g_l, g_u, A_ul):
+    A_gray = A_ul/ (4 * np.pi)
+    l = l.to(u.AA).value
+    return 1.884e-15 * g_u / g_l * l**2 * A_gray.value
+
+def alpha_NaD(l, T, Pg, Pe, micro, which):
+    
+    i = which - 1
+    
+    l0 = np.array([5895.932, 5889.959]) * u.AA
+    A_ul = np.array([0.614, 0.616]) * 10 ** 8 * 1/u.s
+    log_C4 = np.array([-15.33, -15.17])
+    log_C6 = np.array([-31.673, -31.674])
+    
+    g_l = np.array([2, 2]) 
+    g_u = np.array([2, 4])
+    
+    g = gamma(T, Pg, Pe, A_ul[i], log_C4[i], log_C6[i]) / c.c * l0[i]**2
+    Dl = gaussian_width(l0[i], T, micro) 
+
+    a = g /(4 * np.pi * Dl)
+    v = (l - l0[i]) / Dl
+
+    H = Hjerting(v, a)  
+    
+    f = osc_f(l,g_l[i], g_u[i], A_ul[i])
+    
+    return np.sqrt(np.pi) * e**2 / (c.m_e * c.c**2) * f * H / Dl * l0[i]**2
+
+def K_NaD(l, T, Pg, Pe, nH, rho, micro, which):
+    A_Na = 2.14e-6
+    f_e = 2/partition('Na', T, u_table)
+    Phi_Na = saha_phi('Na', T, i_table, u_table)
+    f_i = 1 / (1 + Phi_Na / Pe)
+    
+    alpha = alpha_NaD(l, T, Pg, Pe, micro, which).cgs 
+    stim = 1 - np.exp(-c.h * c.c / (l * c.k_B * T))
+    return alpha * nH / rho * A_Na * f_e * f_i * stim
+
+### HW 11
+
+def opacity(l, T, Pg, Pe, nH, rho, micro):
+    K = Kc_total(l, T, Pg, Pe)
+    for i in range(1, 3):
+        K += K_NaD(l, T, Pg, Pe, nH, rho, micro, i).cgs
+    return K
+
+def tau_fn(l):
+    K = opacity(l, T, Pg, Pe, nH, rho, micro)
+    tau = np.array([trapz(K[:i+1] * rho[:i+1], s[:i+1]) for i in range(len(s))])
+    return tau
